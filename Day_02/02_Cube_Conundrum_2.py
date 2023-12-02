3e6e452c4ceb063372c67cfc701d895de0f6ebcb
Chris Bowles <BowlesCR@users.noreply.github.com>
2023-12-02 00:31:43 -0500

    Refactor, `black`en

--- Day_02/02_Cube_Conundrum_2.py
+++ Day_02/02_Cube_Conundrum_2.py
@@ -1,29 +1,24 @@
 import fileinput
 import re
+from math import prod
 
-re_game = re.compile(r'^Game (\d+): (.+)\n$')
-re_cube = re.compile(r'(\d+) (red|blue|green)')
+re_game = re.compile(r"^Game (\d+): (.+)\n$")
+re_cube = re.compile(r"(\d+) (red|blue|green)")
 
 sum_ = 0
 
 for line in fileinput.input():
     gamenum, gamestr = re_game.match(line).groups()
-    rounds = gamestr.split(';')
+    rounds = gamestr.split(";")
 
-    red = 0
-    green = 0
-    blue = 0
+    counts = {"red": 0, "green": 0, "blue": 0}
 
     for r in rounds:
         for cube in re_cube.findall(r):
-            if cube[1] == 'red':
-                red = max(red, int(cube[0]))
-            elif cube[1] == 'green':
-                green = max(green, int(cube[0]))
-            elif cube[1] == 'blue':
-                blue = max(blue, int(cube[0]))
+            count, color = cube
+            counts[color] = max(counts[color], int(count))
 
-    power = red * blue * green
+    power = prod(counts.values())
     print(power)
     sum_ += power
 
